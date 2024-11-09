725bd4100ce76a5caea6e61e60f9046e4939b85a
KA 💻 <116130909+Exoticbluebear@users.noreply.github.com>
2024-11-08 23:31:58 -0800

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,13 @@
 // src/App.js
 import React from 'react';
 import CarFinanceCalculator from './CarFinanceCalculator';
-//import Background from './Background';
+import Background from './Background';
 import './App.scss'; 
 
 const App = () => {
   return (
-    <div className="app-container">
+    <background />
+    <div className="app-container">   
       <div className="calculator-overlay">
         <CarFinanceCalculator />
       </div>
